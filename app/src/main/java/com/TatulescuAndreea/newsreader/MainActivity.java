a502a30359719d53fbb1da5b85b339ee77c1aba6
andreeatatulescu <andreea.tatulescu2610@gmail.com>
2021-11-08 01:02:38 +0200

    First commit - News Reader

---
+++ app/src/main/java/com/TatulescuAndreea/newsreader/MainActivity.java
@@ -0,0 +1,21 @@
+package com.TatulescuAndreea.newsreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.TatulescuAndreea.newsreader.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/TatulescuAndreea/newsreader/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.TatulescuAndreea.newsreader.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
